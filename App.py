bdc33ca16018194b0a1879148332b8f8be243da2
0-Arngerdur-1 <par.rack@outlook.com>
2020-10-16 20:18:18 -0500

    Added the function of insert data into the database.
    Now the information of the contact form is added to the database

--- App.py
+++ App.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'onl1- F0r'
+app.config['MYSQL_PASSWORD'] = 'password'
 app.config['MYSQL_DB'] = 'contactore'
 mysql = MySQL(app)
 
@@ -13,9 +13,21 @@ mysql = MySQL(app)
 def Index():
     return render_template("index.html")
 
-@app.route('/add_contact')
+@app.route('/add_contact', methods=['POST'])
 def add_contact():
-    return 'add contact'
+    if request.method == 'POST':
+        contact_name = request.form['contact_name']
+        contact_phone = request.form['contact_phone']
+        contact_email = request.form['contact_email']
+        contact_address = request.form['contact_address']
+        contact_relationship = request.form['contact_relationship']
+        contact_pending = request.form['contact_pending']
+        
+        cur = mysql.connection.cursor()
+        cur.execute('INSERT INTO contacts (name, phone, email, address, relationship, pending) VALUES (%s, %s, %s, %s, %s, %s)', (contact_name, contact_phone, contact_email, contact_address, contact_relationship, contact_pending))
+        mysql.connection.commit()
+        return 'contact added!'
+
 
 @app.route('/edit')
 def edit_contact():
