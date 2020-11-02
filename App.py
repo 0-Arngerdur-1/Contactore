2d19a5f8052a8c30e0e4912702597f040986f087
0-Arngerdur-1 <par.rack@outlook.com>
2020-11-01 20:43:55 -0600

    Added front end and the search contacts function

--- App.py
+++ App.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 # MySQL connection
 app.config["MYSQL_HOST"] = "localhost"
 app.config["MYSQL_USER"] = "root"
-app.config["MYSQL_PASSWORD"] = "password"
+app.config["MYSQL_PASSWORD"] = "pass"
 app.config["MYSQL_DB"] = "contactore"
 mysql = MySQL(app)
 
@@ -16,7 +16,7 @@ app.secret_key = "mysecretkey"
 @app.route("/")
 def Index():
     cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM contacts")
+    cur.execute("SELECT * FROM contacts ORDER BY name ASC")
     mysql.connection.commit()
     data = cur.fetchall()
     return render_template("index.html", contacts=data)
@@ -49,13 +49,11 @@ def add_contact():
         return redirect(url_for("Index"))
 
 
-@app.route("/edit/<string:id>")
+@app.route("/edit/<id>")
 def get_contact(id):
     cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM contacts WHERE contactID = (%s)", (id))
-    mysql.connection.commit()
+    cur.execute("SELECT * FROM contacts WHERE contactID = {0}".format(id))
     data = cur.fetchall()
-    print(data[0])
     return render_template("edit_contact.html", contact=data[0])
 
 
@@ -73,13 +71,13 @@ def update_contact(id):
         cur.execute(
             """
         UPDATE contacts
-        SET name = (%s),
-            phone = (%s),
-            email = (%s),
-            address = (%s),
-            relationship = (%s),
-            pending = (%s)
-            WHERE contactID = (%s)
+        SET name = %s,
+            phone = %s,
+            email = %s,
+            address = %s,
+            relationship = %s,
+            pending = %s
+            WHERE contactID = %s
             """,
             (
                 contact_name,
@@ -96,13 +94,27 @@ def update_contact(id):
         return redirect(url_for("Index"))
 
 
-@app.route("/delete_contact/<string:id>")
+@app.route("/delete_contact/<id>")
 def delete_contact(id):
     cur = mysql.connection.cursor()
-    cur.execute("DELETE FROM contacts WHERE contactID = (%s)", (id))
+    cur.execute("DELETE FROM contacts WHERE contactID = {0}".format(id))
     mysql.connection.commit()
     flash("Contacto eliminado")
     return redirect(url_for("Index"))
+
+
+@app.route("/search_contact", methods=["POST"])
+def search_contact():
+    if request.method == "POST":
+        contact_search = request.form["contact_search"]
+        cur = mysql.connection.cursor()
+        cur.execute(
+            "SELECT * FROM contacts WHERE name LIKE '%{0}%'".format(contact_search)
+        )
+        mysql.connection.commit()
+        data = cur.fetchall()
+        print(data)
+        return render_template("search_contact.html", contacts=data)
 
 
 if __name__ == "__main__":
